75814c69ffa4f388b887933ca7217539363513ab
devferx <ferkki@hotmail.com>
2023-01-02 19:24:32 -0400

    Documentación automática con Swagger

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.title = "Movie API"
+app.version = "0.0.1"
 
 
-@app.get("/")
+@app.get("/", tags=["home"])
 def message():
     return "Hello world!"
